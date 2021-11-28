d53634694545b2457c75f36ec0426ec6c1fd7782
David Howell <david@davidjameshowell.com>
2021-11-28 01:04:25 -0700

    Remove types from variables due to TF issues

--- variables.tf
+++ variables.tf
@@ -92,66 +92,21 @@ variable "block_device_mappings" {
 
 variable "instance_market_options" {
   description = "The market (purchasing) option for the instances"
-
-  type = object({
-    market_type = string
-    spot_options = object({
-      block_duration_minutes         = number
-      instance_interruption_behavior = string
-      max_price                      = number
-      spot_instance_type             = string
-      valid_until                    = string
-    })
-  })
-
   default = null
 }
 
 variable "instance_refresh" {
   description = "The instance refresh definition"
-  type = object({
-    strategy = string
-    preferences = object({
-      instance_warmup        = number
-      min_healthy_percentage = number
-    })
-    triggers = list(string)
-  })
-
   default = null
 }
 
 variable "mixed_instances_policy" {
   description = "policy to used mixed group of on demand/spot of differing types. Launch template is automatically generated. https://www.terraform.io/docs/providers/aws/r/autoscaling_group.html#mixed_instances_policy-1"
-
-  type = object({
-    instances_distribution = object({
-      on_demand_allocation_strategy            = string
-      on_demand_base_capacity                  = number
-      on_demand_percentage_above_base_capacity = number
-      spot_allocation_strategy                 = string
-      spot_instance_pools                      = number
-      spot_max_price                           = string
-    })
-    override = list(object({
-      instance_type     = string
-      weighted_capacity = number
-    }))
-  })
   default = null
 }
 
 variable "placement" {
   description = "The placement specifications of the instances"
-
-  type = object({
-    affinity          = string
-    availability_zone = string
-    group_name        = string
-    host_id           = string
-    tenancy           = string
-  })
-
   default = null
 }
 
